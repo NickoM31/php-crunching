f14393200cbac33dfe24cd188b5b455dba98c3b0
Nicko <nicolasmathieu31@hotmail.com>
2017-01-24 16:51:58 +0100

    Nombre de mots qui contiennent la lettre w

--- index.php
+++ index.php
@@ -6,25 +6,33 @@
 </head>
 <body>
 	<?php 
-$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
-$dico = explode("\n", $string);
+	$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
+	$dico = explode("\n", $string);
 // print_r ($dico);
 // print_r(sizeof($dico));
-?>
+	?>
+	<!-- Nombre de mots avec 15 caractères -->
+	<?php 
+	$tabWord = array();
+	foreach ($dico as $word) {
+		if (strlen($word) == 15) {
+			array_push($tabWord, $word);
+		}
+	}
+	echo count($tabWord);
+	?>
+	<!-- Nombre de mots qui contiennent la lettre w -->
 
-<?php 
-$tabWord = array();
-foreach ($dico as $word) {
-    if (strlen($word) == 15) {
-        array_push($tabWord, $word);
-    }
-}
-echo count($tabWord);
- ?>
- 
-
-
-
+	<?php 
+	$tabWord = array();
+// $wLetter = stripos($dico, 'w');
+	foreach ($dico as $letter) {
+		if (stripos($letter, 'w') !== false) {
+			array_push($tabWord, $letter);
+		}
+	}
+	echo count($tabWord);
+	?>
 
 </body>
 </html>
